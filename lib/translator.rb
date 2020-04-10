a80fe9ea103129fc4f0015f336258eeaaad8a1ef
Krysta Wolkowiecki <krystawolkowiecki@outlook.com>
2020-04-10 06:57:52 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -24,7 +24,7 @@ def get_japanese_emoticon(english_meaning)
   emoticons =  load_libray('.lib/emoticons.yml')
   emoticons.each do |e_meaning, value|
     if e_meaning == english_meaning
-      
+
 
 end
 
