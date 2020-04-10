512ad489476813f5cb8cf407f9d39aae8bce2c56
Krysta Wolkowiecki <krystawolkowiecki@outlook.com>
2020-04-10 07:27:24 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -47,7 +47,7 @@ def get_english_meaning(file_path, japanese_emoticon)
     value.each do |language,sign|
       if language.to_s == "japanese"
           if sign == japanese_emoticon
-            english_emoticon = emoticons[key][:english]
+            english_emoticon = emoticons[key]
           end
       end
     end
