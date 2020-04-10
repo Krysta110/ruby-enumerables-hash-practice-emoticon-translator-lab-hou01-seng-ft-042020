e4db70436e9c9e094871ccd3e8d0e6bded220140
Krysta Wolkowiecki <krystawolkowiecki@outlook.com>
2020-04-10 07:12:38 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -24,7 +24,7 @@ def get_japanese_emoticon(file_path, english_emoticon)
   emoticons =  load_library(file_path)
   emoticons.each do |key, value|
     binding.pry
-    value.each do |language|
+    value.each do |language,sign|
       binding.pry
       if language.to_s == "english"
       japanese_emoticon = emoticons[key][:japanese]
